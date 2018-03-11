f3d791806300d177533d74a465ef5309b2b12177
kentthai <kthai2016@gmail.com>
2018-03-10 16:47:55 -0800

    Add files via upload
    File containing the Arduino code

---
+++ Health_Tracker.ino
@@ -0,0 +1,86 @@
+/**
+ * Simple routine that performs the following:
+ *  1. Configures the software UART on pins 2 and 4 (RX,TX)
+ *  2. Increments a 32-bit variable every 500ms
+ *  4. If it receives a '1' character from bluetooth, it toggles an LED
+ *     
+ *  @author Justin Bauer
+ *  @date 4.24.2016
+ */
+/**
+ * Additions:
+ *  1. Counts "steps" from accelerometer
+ *  2. Resets step value based on button command on Android App
+ *  
+ *  @author Kent Thai
+ *  @date 3.10.2018
+ */
+
+#include <Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int steps = 0;
+int16_t lastAcZ = 0;
+
+
+#include <SoftwareSerial.h> // use the software uart
+SoftwareSerial bluetooth(2, 4); // RX, TX
+
+unsigned long previousMillis = 0;        // will store last time
+const long interval = 500;           // interval at which to delay
+static uint32_t tmp; // increment this
+
+void setup() {
+
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  
+  pinMode(13, OUTPUT); // for LED status
+  bluetooth.begin(9600); // start the bluetooth uart at 9600 which is its default
+  delay(200); // wait for voltage stabilize
+  Serial.begin(9600);
+
+  steps = 0;
+  
+}
+
+void loop() {
+
+  if(bluetooth.read() == '1'){
+    // Reset the steps
+    steps = 0;
+    Serial.print("RESET\n");
+  }
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers, 
+  
+   //Values for the acceleration in each direction (We use these three values for this demonstration)
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  //Values for the temperature read from the register
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+   //Values for the orientation in each direction
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  // Calculate whether or not to increment steps
+  if(lastAcZ - AcZ > 3000){
+    steps++;
+  }
+  lastAcZ = AcZ;
+
+  //bluetooth.print("\n Steps = "); 
+  bluetooth.print(steps);
+
+
+  delay(200);
+
+}
